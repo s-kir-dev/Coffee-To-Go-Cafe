a185a648357c08a35f2216f0b4ce54d5bc43d68e
Kirill <syskirill.com>
2024-11-24 01:11:39 +0300

    Initial Commit

---
+++ Coffee To Go Cafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffee To Go Cafe
+//
+//  Created by Кирилл Сысоев on 24.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
